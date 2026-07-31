c2ee1380c6059ed1c7ebb9819784ff9af2f843ab
akashskypatel <akashskypatel@gmail.com>
2026-07-31 11:32:55 -0700

    refactor: separate pipeline implementations

--- src/pipeline/ParallelRemeshPipeline.cpp
+++ src/pipeline/ParallelRemeshPipeline.cpp
@@ -1,2 +1,23 @@
-#define DIRECTIONAL_PARALLEL_REMESH_PIPELINE_IMPLEMENTATION
 #include <directional/pipeline/ParallelRemeshPipeline.h>
+
+namespace directional::pipeline {
+
+RemeshResult parallel_remesh_from_raw_cross_field(
+    const Eigen::MatrixXd &vertices, const Eigen::MatrixXi &faces,
+    const Eigen::MatrixXd &rawCrossField, RemeshOptions options) {
+  options.parallelizeComponents = true;
+  return remesh_from_raw_cross_field(vertices, faces, rawCrossField, options);
+}
+
+} // namespace directional::pipeline
+
+namespace directional::pipeline {
+
+RemeshResult parallel_remesh_from_mesh(
+    const Eigen::MatrixXd &vertices, const Eigen::MatrixXi &faces,
+    RemeshOptions options) {
+  options.parallelizeComponents = true;
+  return remesh_from_mesh(vertices, faces, options);
+}
+
+} // namespace directional::pipeline
